70be3a067b18fde244fb27462bb758303ea1c755
Bartgeier <38810589+bartgeier@users.noreply.github.com>
2022-10-08 18:46:04 +0200

    added tmux configuration and a shellsc

---
+++ ubuntu-post-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# sudo bash ubuntu-post-installer.sh 
+# https://www.addictivetips.com/ubuntu-linux-tips/make-ubuntu-post-installation-script/
+# https://itsfoss.com/install-deb-files-ubuntu/
+
+# sudo apt install program1 program2 program3 -y
+sudo apt install tmux
+
+# https://snapcraft.io/store
+# sudo snap install snappackage1 snappackage2 snappackage3
+
+# https://flathub.org/home
+# sudo flatpak install flatpak1 flatpak2 flatpak3 -y
+
