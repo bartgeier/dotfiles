b875b10efc9d167995a4c6946ad9298a4fa02f7e
Bartgeier <38810589+bartgeier@users.noreply.github.com>
2022-10-11 20:28:28 +0200

    add WhatsApp and Thunderbird to the script

--- .config/mv_win_to_workspace.sh
+++ .config/mv_win_to_workspace.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 # https://sites.google.com/site/tstyblo/wmctrl
-wmctrl -r Skype -t 1
+# screen resolution '0,1920,0,1920,1080'
+
 wmctrl -r Firefox -t 0
+
+wmctrl -r Thunderbird -t 0
+wmctrl -r Thunderbird -b 'remove,fullscreen'
+wmctrl -r Thunderbird -b 'remove,maximized_vert'
+wmctrl -r Thunderbird -b 'remove,maximized_horz'
+wmctrl -r Thunderbird -e '0,2000,200,500,500'
+wmctrl -r Thunderbird -b 'toggle,maximized_vert'
+wmctrl -r Thunderbird -b 'toggle,maximized_horz'
+
+wmctrl -r Skype -t 1
+wmctrl -r WhatsApp -t 2
