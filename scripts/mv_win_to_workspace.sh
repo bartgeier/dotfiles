8830fbd835e03b974c1a0794c9d4342235c3d066
Bartgeier <38810589+bartgeier@users.noreply.github.com>
2022-12-04 10:27:36 +0100

    ctrl-m in desktiop moves windows to screen and workspaces

---
+++ scripts/mv_win_to_workspace.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# https://sites.google.com/site/tstyblo/wmctrl
+# screen resolution '0,1920,0,1920,1080'
+
+screen_one()
+{
+        APP=$1
+        WORKSPACE=$2
+        wmctrl -r "$APP" -t "$WORKSPACE"
+        wmctrl -r "$APP" -b 'remove,fullscreen'
+        wmctrl -r "$APP" -b 'remove,maximized_vert'
+        wmctrl -r "$APP" -b 'remove,maximized_horz'
+        wmctrl -r "$APP" -e '0,500,500,500,500'
+        wmctrl -r "$APP" -b 'toggle,maximized_vert'
+        wmctrl -r "$APP" -b 'toggle,maximized_horz'
+}
+
+screen_two()
+{
+        APP=$1
+        WORKSPACE=$2
+        wmctrl -r "$APP" -t "$WORKSPACE"
+        wmctrl -r "$APP" -b 'remove,fullscreen'
+        wmctrl -r "$APP" -b 'remove,maximized_vert'
+        wmctrl -r "$APP" -b 'remove,maximized_horz'
+        wmctrl -r "$APP" -e '0,2000,200,500,500'
+        wmctrl -r "$APP" -b 'toggle,maximized_vert'
+        wmctrl -r "$APP" -b 'toggle,maximized_horz'
+}
+
+screen_two_left()
+{
+        APP=$1
+        WORKSPACE=$2
+        wmctrl -r "$APP" -t "$WORKSPACE"
+        wmctrl -r "$APP" -b 'remove,fullscreen'
+        wmctrl -r "$APP" -b 'remove,maximized_vert'
+        wmctrl -r "$APP" -b 'remove,maximized_horz'
+        wmctrl -r "$APP" -e '0,1920,0,960,1080'
+        wmctrl -r "$APP" -b 'toggle,maximized_vert'
+#        wmctrl -r $APP -b 'toggle,maximized_horz'
+}
+
+screen_two_right()
+{
+        APP=$1
+        WORKSPACE=$2
+        wmctrl -r "$APP" -t "$WORKSPACE"
+        wmctrl -r "$APP" -b 'remove,fullscreen'
+        wmctrl -r "$APP" -b 'remove,maximized_vert'
+        wmctrl -r "$APP" -b 'remove,maximized_horz'
+        wmctrl -r "$APP" -e '0,2880,0,960,1080'
+        wmctrl -r "$APP" -b 'toggle,maximized_vert'
+#        wmctrl -r $APP -b 'toggle,maximized_horz'
+}
+
+
+screen_one Firefox 0
+screen_two_left WhatsApp 0
+screen_two_right Thunderbird 0
+screen_one Skype 1
